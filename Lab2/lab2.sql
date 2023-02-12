0e6ea142d1ba806cc311ec2103d36457399bea67
groyvstreet <kirya.polyakov.2003@mail.ru>
2023-02-12 18:08:24 +0300

    Lab2 task 2 added

--- Lab2/lab2.sql
+++ Lab2/lab2.sql
@@ -15,7 +15,7 @@ create table groups(
 create or replace trigger students_insert
     before insert
     on students
-    FOR EACH ROW
+    for each row
 declare
     amount number;
     last_id number;
@@ -25,14 +25,33 @@ begin
         :new.id := 1;
     else
         select max(id) into last_id from students;
-        :new.id := last_id + 1;
+        if :new.id > last_id then
+            :new.id := last_id + 1;
+        elsif :new.id > 1 then
+            select count(*) into amount from students where id = :new.id;
+            if amount > 0 then
+                :new.id := last_id + 1;
+            end if;
+        else
+            :new.id := last_id + 1;
+        end if;
     end if;
 end students_insert;
+
+create or replace trigger students_update
+    before update
+    on students
+    for each row
+begin
+    if :old.id != :new.id then
+        RAISE_APPLICATION_ERROR(-20001, 'Student id cannot be changed.');
+    end if;
+end students_update;
 
 create or replace trigger groups_insert
     before insert
     on groups
-    FOR EACH ROW
+    for each row
 declare
     amount number;
     last_id number;
@@ -44,9 +63,34 @@ begin
             :new.id := 1;
         else
             select max(id) into last_id from groups;
-            :new.id := last_id + 1;
+            if :new.id > last_id then
+                :new.id := last_id + 1;
+            elsif :new.id > 1 then
+                select count(*) into amount from groups where id = :new.id;
+                if amount > 0 then
+                    :new.id := last_id + 1;
+                end if;
+            else
+                :new.id := last_id + 1;
+            end if;
         end if;
     else
         RAISE_APPLICATION_ERROR(-20001, 'This group name is already exists.');
     end if;
 end groups_insert;
+
+create or replace trigger groups_update
+    before update
+    on groups
+    for each row
+begin
+    if :old.id != :new.id then
+        RAISE_APPLICATION_ERROR(-20001, 'Group id cannot be changed.');
+    end if;
+
+    if :old.name != :new.name then
+        RAISE_APPLICATION_ERROR(-20001, 'Group name cannot be changed.');
+    end if;
+end groups_update;
+
+-- 3.
