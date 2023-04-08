2c6d1aaa0363133dcd2817eee397efcac39c5295
groyvstreet <kirya.polyakov.2003@mail.ru>
2023-04-08 21:55:52 +0300

    Lab4 updated

--- Lab4/lab4.sql
+++ Lab4/lab4.sql
@@ -1,7 +1,7 @@
 declare
-    json clob;
+    json_text clob;
 begin
-    json := '
+    json_text := '
     {
         "request": "select",
         "columns": [
@@ -12,71 +12,147 @@ begin
             "cars"
         ],
         "conditions": [
-            "id < 3 and",
-            "id = {
-                "request": "select",
-                "columns": "id",
-                "tables": "cars",
-                "conditions": [
-                    "id = 2"
-                ]
-            }"
+            {
+                "type": "default",
+                "condition": "id < 3 and id ="
+            },
+            {
+                "type": "request",
+                "condition": {
+                    "request": "select",
+                    "columns": [
+                        "id"
+                    ],
+                    "tables": [
+                        "cars"
+                    ],
+                    "conditions": [
+                        {
+                            "type": "default",
+                            "condition": "id = 2"
+                        }
+                    ]
+                }
+            }
         ]
     }
     ';
-    execute_request(json);
+    execute_request(json_text);
 end;
 
-create or replace procedure execute_request(json clob) is
-    formatted_json clob;
-    strings_count number;
-    strings dbms_sql.varchar2a;
+create or replace procedure execute_request(json_text clob) is
+    json json_object_t;
+    temp_array json_array_t;
+    temp_object json_object_t;
+
+    request_type clob;
     cols clob;
     tabs clob;
     conditions clob;
+    condition_type clob;
 begin
-    dbms_output.put_line(parse_request(json));
+    json := json_object_t.parse(json_text);
+
+    request_type := json.get_string('request');
+
+    if request_type = 'select' then
+        -- columns
+        temp_array := json.get_array('columns');
+
+        for i in 0..temp_array.get_size() - 1
+        loop
+            if i = temp_array.get_size() - 1 then
+                cols := cols || temp_array.get_string(i);
+            else
+                cols := cols || temp_array.get_string(i) || ', ';
+            end if;
+        end loop;
+
+        -- tables
+        temp_array := json.get_array('tables');
+
+        for i in 0..temp_array.get_size() - 1
+        loop
+            if i = temp_array.get_size() - 1 then
+                tabs := tabs || temp_array.get_string(i);
+            else
+                tabs := tabs || temp_array.get_string(i) || ', ';
+            end if;
+        end loop;
+
+        -- conditions
+        temp_array := json.get_array('conditions');
+
+        for i in 0..temp_array.get_size() - 1
+        loop
+            temp_object := treat(temp_array.get(i) as json_object_t);
+
+            condition_type := temp_object.get_string('type');
+
+            if condition_type = 'default' then
+                conditions := conditions || ' ' || temp_object.get_string('condition');
+            else
+                conditions := conditions || ' (' || parse_request(treat(temp_object.get('condition') as json_object_t)) || ')';
+            end if;
+        end loop;
+
+        dbms_output.put_line('select ' || cols || ' from ' || tabs || ' where' || conditions);
+    end if;
 end execute_request;
 
-create or replace function parse_request(json clob) return clob is
-    formatted_json clob;
-    strings_count number;
-    strings dbms_sql.varchar2a;
+create or replace function parse_request(json json_object_t) return clob is
+    temp_array json_array_t;
+    temp_object json_object_t;
+
+    request_type clob;
     cols clob;
     tabs clob;
     conditions clob;
+    condition_type clob;
 begin
-    formatted_json := replace(json, ' ', '');
-    formatted_json := replace(formatted_json, chr(10) || '{', '{');
-    formatted_json := replace(formatted_json, '}' || chr(10), '}');
-    dbms_output.put_line(formatted_json);
-    strings_count := regexp_count(formatted_json, '(\S*)' || chr(10));
-    for i in 1..strings_count
-    loop
-        strings(i) := replace(regexp_substr(formatted_json, '(\S*)' || chr(10), 1, i), chr(10), '');
-    end loop;
-  
-    for i in 1..strings.count
-    loop
-        dbms_output.put_line(strings(i));
-    end loop;
+    request_type := json.get_string('request');
 
-    if regexp_substr(strings(2), '"(\S*)"', 10, 1) = '"select"' then
-        cols := regexp_substr(strings(3), '"(\S*)"', 10, 1);
-        cols := replace(cols, '"', '');
-        cols := replace(cols, ',', ', ');
-        dbms_output.put_line(cols);
+    if request_type = 'select' then
+        -- columns
+        temp_array := json.get_array('columns');
 
-        tabs := regexp_substr(strings(4), '"(\S*)"', 9, 1);
-        tabs := replace(tabs, '"', '');
-        tabs := replace(tabs, ',', ', ');
-        dbms_output.put_line(tabs);
+        for i in 0..temp_array.get_size() - 1
+        loop
+            if i = temp_array.get_size() - 1 then
+                cols := cols || temp_array.get_string(i);
+            else
+                cols := cols || temp_array.get_string(i) || ', ';
+            end if;
+        end loop;
 
-        conditions := regexp_substr(strings(5), '"(\S*)"', 13, 1);
-        conditions := replace(conditions, '"', '');
-        conditions := replace(conditions, ',', ', ');
-        dbms_output.put_line(conditions);
+        -- tables
+        temp_array := json.get_array('tables');
 
-        dbms_output.put_line('select ' || cols || ' from ' || tabs || ' where ' || conditions);
+        for i in 0..temp_array.get_size() - 1
+        loop
+            if i = temp_array.get_size() - 1 then
+                tabs := tabs || temp_array.get_string(i);
+            else
+                tabs := tabs || temp_array.get_string(i) || ', ';
+            end if;
+        end loop;
+
+        -- conditions
+        temp_array := json.get_array('conditions');
+
+        for i in 0..temp_array.get_size() - 1
+        loop
+            temp_object := treat(temp_array.get(i) as json_object_t);
+
+            condition_type := temp_object.get_string('type');
+
+            if condition_type = 'default' then
+                conditions := conditions || temp_object.get_string('condition');
+            else
+                conditions := conditions || parse_request(treat(temp_object.get('condition') as json_object_t));
+            end if;
+        end loop;
+
+        return 'select ' || cols || ' from ' || tabs || ' where ' || conditions;
     end if;
 end parse_request;
