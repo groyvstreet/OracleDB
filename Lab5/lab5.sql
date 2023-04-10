28cc37bea5746ef12d64396fa7f89ae495e5635b
groyvstreet <kirya.polyakov.2003@mail.ru>
2023-04-10 17:48:21 +0300

    Lab5 task 3 done

--- Lab5/lab5.sql
+++ Lab5/lab5.sql
@@ -46,117 +46,17 @@ begin
     end if;
 end;
 
-create or replace procedure restore_companies(date_time date) is
-    cursor records is
-        select *
-        from companies_logs
-        where action_date = date_time
-        order by id;
-    amount number;
-begin
-    execute immediate 'alter trigger companies_logs disable';
-
-    for record in records
-    loop
-        if record.action = 'insert' or record.action = 'update' then
-            select count(*) into amount from companies where id = record.company_id;
-
-            if amount = 0 then
-                insert into companies values(record.company_id, record.company_name, record.company_opening_date, record.company_cars_amount);
-            else
-                update companies
-                    set name = record.company_name,
-                    opening_date = record.company_opening_date,
-                    cars_amount = record.company_cars_amount
-                    where id = record.company_id;    
-            end if;
-        end if;
-
-        if record.action = 'delete' then
-            delete from companies where id = record.company_id;
-        end if;
-    end loop;
-
-    execute immediate 'alter trigger companies_logs enable';
-end;
-
-create or replace procedure restore_persons(date_time date) is
-    cursor records is
-        select *
-        from persons_logs
-        where action_date = date_time
-        order by id;
-    amount number;
-begin
-    execute immediate 'alter trigger persons_logs disable';
-
-    for record in records
-    loop
-        if record.action = 'insert' or record.action = 'update' then
-            select count(*) into amount from persons where id = record.person_id;
-
-            if amount = 0 then
-                insert into persons values(record.person_id, record.person_name, record.person_birthday, record.person_cars_amount);
-            else
-                update persons
-                    set name = record.person_name,
-                    birthday = record.person_birthday,
-                    cars_amount = record.person_cars_amount
-                    where id = record.person_id;    
-            end if;
-        end if;
-
-        if record.action = 'delete' then
-            delete from persons where id = record.person_id;
-        end if;
-    end loop;
-
-    execute immediate 'alter trigger persons_logs enable';
-end;
-
-create or replace procedure restore_cars(date_time date) is
-    cursor records is
-        select *
-        from cars_logs
-        where action_date = date_time
-        order by id;
-    amount number;
-begin
-    execute immediate 'alter trigger cars_logs disable';
-
-    for record in records
-    loop
-        if record.action = 'insert' or record.action = 'update' then
-            select count(*) into amount from cars where id = record.car_id;
-
-            if amount = 0 then
-                insert into cars values(record.car_id, record.car_model_name, record.car_manufacture_date, record.car_company_id, record.car_person_id);
-            else
-                update cars
-                    set model_name = record.car_model_name,
-                    manufacture_date = record.car_manufacture_date,
-                    company_id = record.car_company_id,
-                    person_id = record.car_person_id
-                    where id = record.car_id;    
-            end if;
-        end if;
-
-        if record.action = 'delete' then
-            delete from cars where id = record.car_id;
-        end if;
-    end loop;
-
-    execute immediate 'alter trigger cars_logs enable';
-end;
-
 create or replace package restore_data is
     procedure restore_companies(date_time date);
     procedure restore_persons(date_time date);
     procedure restore_cars(date_time date);
 
-    procedure restore_companies(mls1 number, mls2 number);
-    procedure restore_persons(mls1 number, mls2 number);
-    procedure restore_cars(mls1 number, mls2 number);
+    procedure restore_companies(mls number);
+    procedure restore_persons(mls number);
+    procedure restore_cars(mls number);
+
+    procedure restore_data(date_time date);
+    procedure restore_data(mls number);
 end restore_data;
 
 create or replace package body restore_data is
@@ -164,11 +64,12 @@ create or replace package body restore_data is
         cursor records is
         select *
         from companies_logs
-        where action_date = date_time
+        where action_date <= date_time
         order by id;
         amount number;
     begin
         execute immediate 'alter trigger companies_logs disable';
+        delete from companies;
 
         for record in records
         loop
@@ -198,11 +99,12 @@ create or replace package body restore_data is
         cursor records is
             select *
             from persons_logs
-            where action_date = date_time
+            where action_date <= date_time
             order by id;
         amount number;
     begin
         execute immediate 'alter trigger persons_logs disable';
+        delete from persons;
 
         for record in records
         loop
@@ -232,11 +134,12 @@ create or replace package body restore_data is
         cursor records is
         select *
         from cars_logs
-        where action_date = date_time
+        where action_date <= date_time
         order by id;
         amount number;
     begin
         execute immediate 'alter trigger cars_logs disable';
+        delete from cars;
 
         for record in records
         loop
@@ -263,43 +166,40 @@ create or replace package body restore_data is
         execute immediate 'alter trigger cars_logs enable';
     end;
 
-    procedure restore_companies(mls1 number, mls2 number) is
+    procedure restore_companies(mls number) is
         date_time date;
     begin
-        for i in mls1..mls2
-        loop
-            select systimestamp - numtodsinterval(i / 1000, 'second')
-            into date_time
-            from dual;
-
-            restore_companies(date_time);
-        end loop;
-    end;
-
-    procedure restore_persons(mls1 number, mls2 number) is
-        date_time1 date;
-        date_time2 date;
-        temp_cursor sys_refcursor;
-    begin
-        select systimestamp - numtodsinterval(mls1 / 1000, 'second')
-        into date_time1
+        select systimestamp - numtodsinterval(mls / 1000, 'second')
+        into date_time
         from dual;
 
-        select systimestamp - numtodsinterval(mls2 / 1000, 'second')
-        into date_time2
-        from dual;
+        restore_companies(date_time);
     end;
 
-    procedure restore_cars(mls1 number, mls2 number) is
+    procedure restore_persons(mls number) is
         date_time date;
     begin
-        for i in mls1..mls2
-        loop
-            select systimestamp - numtodsinterval(i / 1000, 'second')
-            into date_time
-            from dual;
+        select systimestamp - numtodsinterval(mls / 1000, 'second')
+        into date_time
+        from dual;
 
-            restore_cars(date_time);
-        end loop;
+        restore_persons(date_time);
+    end;
+
+    procedure restore_cars(mls number) is
+        date_time date;
+    begin
+        select systimestamp - numtodsinterval(mls / 1000, 'second')
+        into date_time
+        from dual;
+
+        restore_cars(date_time);
+    end;
+
+    procedure restore_data(date_time date) is
+    begin
+        restore_companies(date_time);
+        restore_persons(date_time);
+        restore_cars(date_time);
     end;
 end restore_data;
