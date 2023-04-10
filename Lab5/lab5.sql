473758f3b28ed5495f836876411cf80682fe7ab0
groyvstreet <kirya.polyakov.2003@mail.ru>
2023-04-10 16:27:16 +0300

    Lab5 updated

--- Lab5/lab5.sql
+++ Lab5/lab5.sql
@@ -45,3 +45,225 @@ begin
         insert into cars_logs values(cars_logs_sequence.nextval, 'delete', sysdate, :old.id, :old.model_name, :old.manufacture_date, :old.company_id, :old.person_id);
     end if;
 end;
+
+create or replace procedure restore_companies(date_time date) is
+    cursor records is
+        select *
+        from companies_logs
+        where action_date = date_time
+        order by id;
+    amount number;
+begin
+    execute immediate 'alter trigger companies_logs disable';
+
+    for record in records
+    loop
+        if record.action = 'insert' or record.action = 'update' then
+            select count(*) into amount from companies where id = record.company_id;
+
+            if amount = 0 then
+                insert into companies values(record.company_id, record.company_name, record.company_opening_date, record.company_cars_amount);
+            else
+                update companies
+                    set name = record.company_name,
+                    opening_date = record.company_opening_date,
+                    cars_amount = record.company_cars_amount
+                    where id = record.company_id;    
+            end if;
+        end if;
+
+        if record.action = 'delete' then
+            delete from companies where id = record.company_id;
+        end if;
+    end loop;
+
+    execute immediate 'alter trigger companies_logs enable';
+end;
+
+create or replace procedure restore_persons(date_time date) is
+    cursor records is
+        select *
+        from persons_logs
+        where action_date = date_time
+        order by id;
+    amount number;
+begin
+    execute immediate 'alter trigger persons_logs disable';
+
+    for record in records
+    loop
+        if record.action = 'insert' or record.action = 'update' then
+            select count(*) into amount from persons where id = record.person_id;
+
+            if amount = 0 then
+                insert into persons values(record.person_id, record.person_name, record.person_birthday, record.person_cars_amount);
+            else
+                update persons
+                    set name = record.person_name,
+                    birthday = record.person_birthday,
+                    cars_amount = record.person_cars_amount
+                    where id = record.person_id;    
+            end if;
+        end if;
+
+        if record.action = 'delete' then
+            delete from persons where id = record.person_id;
+        end if;
+    end loop;
+
+    execute immediate 'alter trigger persons_logs enable';
+end;
+
+create or replace procedure restore_cars(date_time date) is
+    cursor records is
+        select *
+        from cars_logs
+        where action_date = date_time
+        order by id;
+    amount number;
+begin
+    execute immediate 'alter trigger cars_logs disable';
+
+    for record in records
+    loop
+        if record.action = 'insert' or record.action = 'update' then
+            select count(*) into amount from cars where id = record.car_id;
+
+            if amount = 0 then
+                insert into cars values(record.car_id, record.car_model_name, record.car_manufacture_date, record.car_company_id, record.car_person_id);
+            else
+                update cars
+                    set model_name = record.car_model_name,
+                    manufacture_date = record.car_manufacture_date,
+                    company_id = record.car_company_id,
+                    person_id = record.car_person_id
+                    where id = record.car_id;    
+            end if;
+        end if;
+
+        if record.action = 'delete' then
+            delete from cars where id = record.car_id;
+        end if;
+    end loop;
+
+    execute immediate 'alter trigger cars_logs enable';
+end;
+
+create or replace package restore_data is
+    procedure restore_companies(date_time date);
+    procedure restore_persons(date_time date);
+    procedure restore_cars(date_time date);
+
+    procedure restore_companies(mls number);
+    procedure restore_persons(mls number);
+    procedure restore_cars(mls number);
+end restore_data;
+
+create or replace package body restore_data is
+    procedure restore_companies(date_time date) is
+        cursor records is
+        select *
+        from companies_logs
+        where action_date = date_time
+        order by id;
+        amount number;
+    begin
+        execute immediate 'alter trigger companies_logs disable';
+
+        for record in records
+        loop
+            if record.action = 'insert' or record.action = 'update' then
+                select count(*) into amount from companies where id = record.company_id;
+
+                if amount = 0 then
+                    insert into companies values(record.company_id, record.company_name, record.company_opening_date, record.company_cars_amount);
+                else
+                    update companies
+                        set name = record.company_name,
+                        opening_date = record.company_opening_date,
+                        cars_amount = record.company_cars_amount
+                        where id = record.company_id;    
+                end if;
+            end if;
+
+            if record.action = 'delete' then
+                delete from companies where id = record.company_id;
+            end if;
+        end loop;
+
+        execute immediate 'alter trigger companies_logs enable';
+    end;
+    
+    procedure restore_persons(date_time date) is
+        cursor records is
+            select *
+            from persons_logs
+            where action_date = date_time
+            order by id;
+        amount number;
+    begin
+        execute immediate 'alter trigger persons_logs disable';
+
+        for record in records
+        loop
+            if record.action = 'insert' or record.action = 'update' then
+                select count(*) into amount from persons where id = record.person_id;
+
+                if amount = 0 then
+                    insert into persons values(record.person_id, record.person_name, record.person_birthday, record.person_cars_amount);
+                else
+                    update persons
+                        set name = record.person_name,
+                        birthday = record.person_birthday,
+                        cars_amount = record.person_cars_amount
+                        where id = record.person_id;    
+                end if;
+            end if;
+
+            if record.action = 'delete' then
+                delete from persons where id = record.person_id;
+            end if;
+        end loop;
+
+        execute immediate 'alter trigger persons_logs enable';
+    end;
+
+    procedure restore_cars(date_time date) is
+        cursor records is
+        select *
+        from cars_logs
+        where action_date = date_time
+        order by id;
+        amount number;
+    begin
+        execute immediate 'alter trigger cars_logs disable';
+
+        for record in records
+        loop
+            if record.action = 'insert' or record.action = 'update' then
+                select count(*) into amount from cars where id = record.car_id;
+
+                if amount = 0 then
+                    insert into cars values(record.car_id, record.car_model_name, record.car_manufacture_date, record.car_company_id, record.car_person_id);
+                else
+                    update cars
+                        set model_name = record.car_model_name,
+                        manufacture_date = record.car_manufacture_date,
+                        company_id = record.car_company_id,
+                        person_id = record.car_person_id
+                        where id = record.car_id;    
+                end if;
+            end if;
+
+            if record.action = 'delete' then
+                delete from cars where id = record.car_id;
+            end if;
+        end loop;
+
+        execute immediate 'alter trigger cars_logs enable';
+    end;
+
+    procedure restore_companies(mls number);
+    procedure restore_persons(mls number);
+    procedure restore_cars(mls number);
+end restore_data;
